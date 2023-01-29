7b669d967bb60932e74925623056e745cb930dc1
ULUNTA <59401825+ULUNTA@users.noreply.github.com>
2023-01-29 09:33:41 +0900

    Change Testnet properties

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const symbol = require('/node_modules/symbol-sdk')
 
-const GENERATION_HASH = '7FCCD304802016BEBBCD342A332F91FF1F3BB5E902988B352697BE245F48E836'
-const EPOCH = 1637848847
-const XYM_ID = '3A8416DB2D53B6C8'
-const NODE_URL = 'https://sym-test.opening-line.jp:3001'
+const GENERATION_HASH = '49D6E1CE276A85B70EAFE52349AACCA389302E7A9754BCF1221E79494FC665A4'
+const EPOCH = 1667250467
+const XYM_ID = '72C0212E67A08BCE'
+const NODE_URL = 'https://sym-test-04.opening-line:3001'
 const NET_TYPE = symbol.NetworkType.TEST_NET
 
 const repositoryFactory = new symbol.RepositoryFactoryHttp(NODE_URL)
